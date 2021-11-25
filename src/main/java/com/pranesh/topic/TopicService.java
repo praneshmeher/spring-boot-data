b3313cb5b7e48e77dfef84ab65f356bda2b7e103
praneshmeher <praneshmeher@gmail.com>
2021-11-25 22:09:32 +0530

    commit

---
+++ src/main/java/com/pranesh/topic/TopicService.java
@@ -0,0 +1,44 @@
+package com.pranesh.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("javascript", "Javascript", "Javascript Description")
+			)); 
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(topic->topic.getId().equals(id)).findFirst().get();
+		
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		for(int i=0; i<topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id))
+				topics.set(i, topic);
+			return;
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(topic->topic.getId().equals(id));
+		
+	}
+}
